86d6842109257257b519aa2059b5d23400ab9a0d
thomaskaviani <thomaskaviani@gmail.com>
2023-10-28 18:38:07 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 npm install &&
-touch /home/atlas/Desktop/npm_installed.txt &&
-npm run flyway-migrate && 
-touch /home/atlas/Desktop/flyway_migrated.txt &&
-npx tsc && 
-touch /home/atlas/Desktop/npx_tsc_complete.txt &&
+npm run flyway-migrate &&
+npx tsc &&
 npm start
