38848f62d58a5838694b08151a5743a0829876b0
tkaviani <tkaviani@mazdaeur.com>
2023-10-31 14:49:12 +0100

    remove msg from logging callback functions

--- src/services/LoggingService.ts
+++ src/services/LoggingService.ts
@@ -19,13 +19,11 @@ export class LoggingService {
             });
         }
         let logMessage = Date.now() + "_" + command + "_" + user + ":" + error;
-        require('child_process').exec("echo '" + logMessage + "' >> /opt/logfile.txt", function (msg) {
-        });
+        require('child_process').exec("echo '" + logMessage + "' >> /opt/logfile.txt", function () {});
     }
 
     public static async log(message: string) {
         let logMessage = Date.now() + ":" + message;
-        require('child_process').exec("echo '" + logMessage + "' >> /opt/logfile.txt", function (msg) {
-        });
+        require('child_process').exec("echo '" + logMessage + "' >> /opt/logfile.txt", function () {});
     }
 }
